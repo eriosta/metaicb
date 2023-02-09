51eb6dce8b8df181a8952397fadb707d1369f39b
Eri Osta <eri@EGOMAC.local>
2023-02-08 23:47:29 -0600

    update regex

--- README.md
+++ README.md
@@ -7,4 +7,8 @@ pip install pymed elsapy pybliometrics
 2. +/- chemo combo
 3. Metastatic versus localized cancer (systemic versus adjuvant therapy) 
 
+https://jamanetwork.com/journals/jamaoncology/fullarticle/2757522
+
+
+Sup: https://cdn.jamanetwork.com/ama/content_public/journal/oncology/938409/coi190099supp1_prod.pdf?Expires=1678837078&Signature=A62KHPYf~gTi7yb9oDzT2VlrkLbYnuApUpy~C3IF2q1E-yS726Y-qH2c7~U-HjvMTAcoKFp5MiX5n821oS4nmO2jmEDOYks~yjlD6gRIUyK8CYs76occegOEv17NhAmVa9tsZLHkANkCO1xcz9gZGWvPKmo4jWbD0bvbr9DwoyRZIfxCs~DqsgV4DHqIEGsQoHcgRkQls8oxlF3kRC9GY0~~~fFbLNiZODmEiXKLLCcROuNBvCerR5XIYOum8MYpc1jll5bpkQGTOt2HolKGYjdGmYNJuqzeNIl7cIFNfEMQVAVQRKMPLmuvFEFk2tGSH2Hxs440CnBLuU42jZWWLw__&Key-Pair-Id=APKAIE5G5CRDK6RD3PGA
 
--- main.py
+++ main.py
@@ -1,53 +1,83 @@
 from pytrials.client import ClinicalTrials
-import pprint as pp
+import re
 import pandas as pd
 # https://pytrials.readthedocs.io/en/latest/_modules/pytrials/client.html#ClinicalTrials.get_study_fields
 
 ct = ClinicalTrials()
 
-# Get 50 full studies related to Coronavirus and COVID in json format.
-# response = ct.get_full_studies(search_expr="Immune-checkpoint inhibitor+PD-L1+PD-1", max_studies=50)
+fields = ["NCTId", "Condition", "BriefTitle", "OfficialTitle","Keyword","Phase",
+            "WhyStopped","PrimaryOutcomeDescription","PrimaryOutcomeMeasure","IsFDARegulatedDrug",
+            "InterventionDescription","InterventionName","InterventionOtherName","InterventionType",
+            "EligibilityCriteria","EnrollmentCount","StartDate",'LastKnownStatus',"LastUpdatePostDate","ReferenceCitation"]
 
-# for k,v in response.items():
-#     for a,b in v.items():
-#         pp.pprint(b)
-#     break
-
-# for field in ct.study_fields:
-#     print(field)
-
-for i in list(ct.study_fields):
-    print(i)
-    
-search="immune checkpoint inhibitor"
-# search="(Immune-checkpoint inhibitor+PD-L1+PD-1)"
+search="(Immune-checkpoint inhibitor+PD-L1+PD-1)"
 
 # Get the NCTId, Condition and Brief title fields from 500 studies related to Coronavirus and Covid, in csv format.
 tmp = ct.get_study_fields(
     search_expr=search,
-    fields=["NCTId", "Condition", "BriefTitle", "OfficialTitle","Keyword","Phase",
-            "WhyStopped","PrimaryOutcomeDescription","PrimaryOutcomeMeasure","IsFDARegulatedDrug",
-            "InterventionDescription","InterventionName","InterventionOtherName","InterventionType",
-            "EligibilityCriteria","EnrollmentCount","StartDate",'LastKnownStatus',"LastUpdatePostDate","ReferenceCitation"],
-    max_studies=3000,
+    fields=fields,
+    max_studies=1000,
     fmt="csv")
 # Get the count of studies related to Coronavirus and COVID.
 # ClinicalTrials limits API queries to 1000 records
 # Count of studies may be useful to build loops when you want to retrieve more than 1000 records
 
-search="immune checkpoint inhibitor"
-# ct.get_study_count(search_expr=search)
+tmp = pd.DataFrame.from_records(tmp[1:], columns=tmp[0])#.to_csv("ClinicalTrials_ICB_PDL1_PD1_N_246.csv")
 
-pd.DataFrame.from_records(tmp[1:], columns=tmp[0]).to_csv("ClinicalTrials_ICB_PDL1_PD1_N_246.csv")
+#pd1
+r'PD-1|PD1|Programmed death-1|Programmed death 1|nivolumab|opdivo|ono-4538|MDX-1106|BMS-936558|nivo|pembrolizumab|keytruda|SCH900475|MK-3475|lambrolizumab'
+# pdl1
+r'Programmed death ligand-1|Programmed death ligand 1|PD-L1|PDL1|atezolizumab|tecentriq|RO5541267|MPDL3280A|RG7446|durvalumab|imfinzi|MED 4736|MED-4736|avelumab|MSB0010718C|cemiplimab|libtayo|REGN2810'
 
-import re
+pd1_pattern = re.compile(r'PD-1|PD1|Programmed death-1|nivolumab|opdivo|ono-4538|MDX-1106|BMS-936558|nivo|pembrolizumab|keytruda|SCH900475|MK-3475|lambrolizumab', re.IGNORECASE)
+pdl1_pattern = re.compile(r'Programmed death ligand-1|PD-L1|PDL1|atezolizumab|tecentriq|RO5541267|MPDL3280A|RG7446|durvalumab|imfinzi|MED 4736|MED-4736|avelumab|MSB0010718C|cemiplimab|libtayo|REGN2810', re.IGNORECASE)
 
-keywords = "nivolumab|opdivo|ono-4538|MDX-1106|BMS-936558|nivo|pembrolizumab|lambrolizumab|keytruda|SCH900475|MK-3475|atezolizumab|tecentriq|RO5541267|RG7446|MPDL3280A|durvalumab|imfinzi|MEDI-4736|MEDI4736|avelumab|barvencik|MSB0010718C|cemiplimab|libtayo|REGN2810"
-pattern = re.compile(r"\b(" + keywords + ")\b")
+regex_list = [pd1_pattern,pdl1_pattern]
+
+# create list of regular expressions
+# regex_list = [re.compile(r'hello|hi', re.IGNORECASE), re.compile(r'goodbye|goodnight', re.IGNORECASE)]
+
+# create function to check for match with regex
 
 
-ct.get_study_count(search_expr=search)
 
 
-keywords = "Programmed death-1|PD-1|PD1|Programmed death ligand-1|PD-L1|PDL1|checkpoint inhibitor|checkpoint blockade"
-pattern = re.compile(r"\b(" + keywords + ")\b")
+def check_match(text, regex_list):
+    istrueforregex1 = False
+    istrueforregex2 = False
+    if regex_list[0].search(text):
+        istrueforregex1 = True
+    if regex_list[1].search(text):
+        istrueforregex2 = True
+    if istrueforregex1 and istrueforregex2:
+        return "Both"
+    elif istrueforregex1:
+        return "AntiPD1"
+    elif istrueforregex2:
+        return "AntiPDL1"
+    else:
+        return "None"
+
+def match_regex(df, columns, regex_list):
+    # Compile the regular expressions for faster matching
+    regex1 = regex_list[0]
+    regex2 = regex_list[1]
+
+    for column in columns:
+        df[column + "_result"] = "None"
+        for index, row in df.iterrows():
+            text = row[column]
+            result = check_match(text, [regex1, regex2])
+            df.at[index, column + "_result"] = result
+
+    return df
+
+regex_list = [pd1_pattern,pdl1_pattern]
+columns = [col for col in tmp.columns if col.startswith("Intervention")]
+result = match_regex(tmp, columns, regex_list)
+print(result)
+result.to_csv("ClinicalTrials_ICB_PDL1_PD1_N_246.csv")
+
+
+# tmp.to_csv("ClinicalTrials_ICB_PDL1_PD1_N_246.csv")
+
